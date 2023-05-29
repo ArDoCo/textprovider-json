e34c6fb87d15639005d3362e25fae7c3ed436fbe
laura <laura.erb1@gmx.de>
2023-05-29 15:06:36 +0200

    added ConverterUtil

---
+++ src/main/java/io/github/ardoco/textproviderjson/converter/ConverterUtil.java
@@ -0,0 +1,33 @@
+package io.github.ardoco.textproviderjson.converter;
+
+import edu.kit.kastel.mcse.ardoco.core.api.data.text.Phrase;
+
+import java.util.List;
+
+/**
+ * This utility class provides methods for the converter
+ */
+public final class ConverterUtil {
+
+    private ConverterUtil() {
+    }
+
+    /**
+     * gets the direct children of the given phrase
+     * @param parentPhrase  the phrase
+     * @return              the direct children of this phrase
+     */
+    public static List<Phrase> getChildPhrases(Phrase parentPhrase) {
+        List<Phrase> subphrases = parentPhrase.getSubPhrases().toList();
+        return subphrases.stream().filter(x -> isPhraseOnHighestLevel(subphrases, x)).toList();
+    }
+
+    private static boolean isPhraseOnHighestLevel(List<Phrase> subphrases, Phrase childPhrase) {
+        for (Phrase subphrase: subphrases) {
+            if (subphrase.isSubPhraseOf(childPhrase)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
